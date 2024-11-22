5c4304848dd2f727d509c53895692af779ff1254
tori-27 <82616652+tori-27@users.noreply.github.com>
2024-11-22 15:53:04 +0100

    feat: add base structure

---
+++ tic-tak-toe-online/src/app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/shared/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="lg" variant='destructive'>Hello</Button>
+    </div>
+  );
+}
