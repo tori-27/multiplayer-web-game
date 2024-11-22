245bfc8bf9d22fab359b1eec3e8169c4124682f6
tori-27 <82616652+tori-27@users.noreply.github.com>
2024-11-22 17:22:59 +0100

    feat: prettier

--- tic-tak-toe-online/src/app/page.tsx
+++ tic-tak-toe-online/src/app/page.tsx
@@ -1,10 +1,23 @@
-import {Button} from "@/shared/ui/button";
+import { prisma } from "@/shared/lib/db";
+import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log("hello" + games);
   return (
     <div>
-      <Button size="lg" variant='destructive'>Hello</Button>
+      <Button size="lg" variant="destructive">
+        Hello
+      </Button>
+
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
